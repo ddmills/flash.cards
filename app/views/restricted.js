a7346b684051d7c87cff878d4b9ec848f19e40ab
dmills <dmills@iastate.edu>
2014-12-13 20:01:59 -0600

    fixed restricted view from not being shown

--- app/views/restricted.js
+++ app/views/restricted.js
@@ -2,7 +2,7 @@
   /*******************************************
    *  RESTRICTED DECK EDIT VIEW              *
    *******************************************/
-  Restricted = Backbone.View.extend({
+  App.Views.Restricted = Backbone.View.extend({
     template: _.template($('#view-restricted-template').html()),
     render: function() { this.$el.html(this.template()); }
   });
