84d3aab34e1c5dfd0ed9d1161beaf2b2b3c0d009
dmills <dmills@iastate.edu>
2014-12-21 20:25:15 -0600

    basic view deck page

--- app/views/view.js
+++ app/views/view.js
@@ -6,11 +6,27 @@
     template: _.template($('#view-viewer-template').html()),
     initialize: function(id) {
       this.model = new App.Models.Deck({ deck_id: id });
-      this.listenTo(this.model, 'sync', function() { console.log('fetched'); });
+      this.listenToOnce(this.model, 'sync', this.render);
+      this.subViews = {
+        'cards' : new App.Views.ViewerCards({ collection: this.model.get('cards') })
+      },
       this.model.fetch();
     },
     render: function() {
-      this.$el.html(this.template(this.model.toJSON));
+      this.$el.html(this.template(this.model.toJSON()));
+      this.subViews.cards.$el = this.$('.view-viewerCards-container');
+      this.model.get('cards').fetch();
+      return this;
+    }
+  });
+
+  App.Views.ViewerCards = Backbone.View.extend({
+    template: _.template($('#view-viewer-cards-template').html()),
+    initialize: function() {
+      this.listenToOnce(this.collection, 'sync', this.render);
+    },
+    render: function() {
+      this.$el.html(this.template({ cards: this.collection.toJSON() }));
       return this;
     }
   });
